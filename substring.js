3fc3152137db6a662221f9ae28f051bc63e4543e
Aaron Williams <aaronrw4@yahoo.com>
2021-08-24 21:27:35 -0700

    first commit

---
+++ substring.js
@@ -0,0 +1,27 @@
+var findSubstring = function(s, words) {
+    let wordsLength = words[0].length
+    let solution = []
+
+    for (i = 0; i < s.length; i++) {
+        let tempArr = [...words]
+        let count = 0
+        let index = 0 + i
+
+        while (count < words.length) {
+            if (tempArr.includes(s.slice(index, index + wordsLength))) {
+                tempArr.splice(tempArr.indexOf(s.slice(index, index + wordsLength)), 1)
+            } else {
+                break
+            }
+            index = index + wordsLength
+            count++
+        }
+
+        if (tempArr.length === 0) {
+            solution.push(i)
+        }
+    }
+    return solution
+};
+
+console.log(findSubstring("wordgoodgoodgoodbestword", ["word","good","best","good"]))
